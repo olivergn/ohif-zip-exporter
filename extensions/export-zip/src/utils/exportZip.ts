6eca7d8e037381b7a93372e67c5e8ba2da0fd4ca
OliverGN <129585487+OliverGN@users.noreply.github.com>
2025-07-28 13:45:14 +1200

    Allow export-zip button to export placeholder zip

--- extensions/export-zip/src/utils/exportZip.ts
+++ extensions/export-zip/src/utils/exportZip.ts
@@ -1,3 +1,14 @@
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
+
 export default async function exportZip({ servicesManager }) {
   console.log('Export ZIP button clicked!');
+
+  const zip = new JSZip();
+
+  zip.file("Hello.txt", "Hello World\n");
+
+  zip.generateAsync({ type:"blob" }).then(function(content) {
+    saveAs(content, "report.zip");
+  });
 };
