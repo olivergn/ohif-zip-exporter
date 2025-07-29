dbf560d9a0c874f5c8ffce8d7e78d6441ffbeff7
OliverGN <129585487+OliverGN@users.noreply.github.com>
2025-07-29 12:14:31 +1200

    Export metadata about patient and study

--- extensions/export-zip/src/utils/exportZip.ts
+++ extensions/export-zip/src/utils/exportZip.ts
@@ -1,12 +1,40 @@
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
 
+// DicomMetadataStore contains all relevant metadata
+import { DicomMetadataStore } from '@ohif/core';
+
 export default async function exportZip({ servicesManager }) {
-  console.log('Export ZIP button clicked!');
+  // Relevant services for retrieving information about viewport
+  const { DisplaySetService, ViewportGridService } = servicesManager.services;
 
+  // Get relevant display set UID for this viewport
+  const { viewports } = ViewportGridService.getState();
+  const displaySetInstanceUID = viewports?.entries()?.next()?.value?.[1]?.displaySetInstanceUIDs?.[0];
+
+  // If no such UID exists, we won't be able to retrieve the metadata
+  if (!displaySetInstanceUID) {
+    console.error('No displaySetInstanceUID found.');
+    return;
+  }
+
+  // Use display set UID to get study UID
+  const studyInstanceUID = DisplaySetService.getDisplaySetByUID(displaySetInstanceUID)?.StudyInstanceUID;
+
+  // Get relevant metadata from study UID
+  const studyMetadata = DicomMetadataStore.getStudy(studyInstanceUID);
+  const patientName = studyMetadata?.series?.[0]?.instances?.[0]?.PatientName?.Alphabetic;
+  const studyDate = studyMetadata?.series?.[0]?.instances?.[0]?.StudyDate;
+
+  // Create JSON structure containing relevant metadata
+  const metadata = {
+    PatientName: patientName,
+    StudyDate: studyDate,
+  };
+
+  // Exporting .zip file using JSZip library
   const zip = new JSZip();
-
-  zip.file("Hello.txt", "Hello World\n");
+  zip.file('metadata.json', JSON.stringify(metadata));
 
   zip.generateAsync({ type:"blob" }).then(function(content) {
     saveAs(content, "report.zip");
