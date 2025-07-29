34c26ba322a80c51dda7e776f797089fc2147b94
OliverGN <129585487+OliverGN@users.noreply.github.com>
2025-07-29 17:03:38 +1200

    Export .jpeg image to .zip

--- extensions/export-zip/src/utils/exportZip.ts
+++ extensions/export-zip/src/utils/exportZip.ts
@@ -1,17 +1,17 @@
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
 
-import cornerstone from '@ohif/extension-cornerstone';
-
 // DicomMetadataStore contains all relevant metadata
 import { DicomMetadataStore } from '@ohif/core';
 
 export default async function exportZip({ servicesManager }) {
   // Relevant services for retrieving information about viewport
-  const { DisplaySetService, ViewportGridService } = servicesManager.services;
+  const { CornerstoneViewportService, DisplaySetService, ViewportGridService } = servicesManager.services;
+
+  // Get data on viewports and active viewport
+  const { viewports, activeViewportId } = ViewportGridService.getState();
 
   // Get relevant display set UID for the active viewport
-  const { viewports, activeViewportId } = ViewportGridService.getState();
   const displaySetInstanceUID = viewports?.get(activeViewportId)?.displaySetInstanceUIDs?.[0];
 
   // If no such UID exists, we won't be able to retrieve the metadata
@@ -21,7 +21,8 @@ export default async function exportZip({ servicesManager }) {
   }
 
   // Use display set UID to get study UID
-  const studyInstanceUID = DisplaySetService.getDisplaySetByUID(displaySetInstanceUID)?.StudyInstanceUID;
+  const displaySet = DisplaySetService.getDisplaySetByUID(displaySetInstanceUID);
+  const studyInstanceUID = displaySet?.StudyInstanceUID;
 
   // Get relevant metadata from study UID
   const studyMetadata = DicomMetadataStore.getStudy(studyInstanceUID);
@@ -34,9 +35,30 @@ export default async function exportZip({ servicesManager }) {
     StudyDate: studyDate,
   };
 
-  // Exporting .zip file using JSZip library
+  // Exporting metadata using JSZip library
   const zip = new JSZip();
   zip.file('metadata.json', JSON.stringify(metadata));
+
+  // Get the viewport element which matches the viewport id
+  const viewportElement = document.querySelector(`[data-viewport-uid='${activeViewportId}']`);
+  const canvasElement = viewportElement?.firstElementChild?.querySelector('canvas');
+
+  // If no such canvas exists, we won't be able to retrieve the image
+  if (!canvasElement) {
+    console.warn('Active viewport contains no canvas.');
+  } else {
+    // Otherwise, we can use the asynchronous toBlob method
+    await new Promise<void>((resolve) => {
+      canvasElement.toBlob(blob => {
+        if (blob) {
+          zip.file('image.jpeg', blob);
+        } else {
+          console.warn('Could not convert canvas to .jpeg.');
+        }
+        resolve();
+      }, 'image/jpeg', 0.95);
+    });
+  }
 
   zip.generateAsync({ type:"blob" }).then(function(content) {
     saveAs(content, "report.zip");
